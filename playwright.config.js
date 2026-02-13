6ec5428a2b62227e030b287227313341202c3291
Viswa <bvsukanth@gmail.com>
2026-02-12 23:02:15 -0600

    First commit message

---
+++ playwright.config.js
@@ -0,0 +1,31 @@
+// @ts-check
+import { defineConfig, devices } from '@playwright/test';
+
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+// import dotenv from 'dotenv';
+// import path from 'path';
+// dotenv.config({ path: path.resolve(__dirname, '.env') });
+
+/**
+ * @see https://playwright.dev/docs/test-configuration
+ */
+const config = ({
+  testDir: './tests',
+  timeout : 40*1000, //default is 30 seconds in playwright
+  expect:{
+    timeout : 5000
+  },
+  use: {
+    browserName : 'chromium',
+    headless : false
+  },
+
+  reporter:'html'
+  
+});
+
+module.exports = config
+
---
+++ tests/UIBasics.spec.js
@@ -0,0 +1,34 @@
+const {test, expect} = require('@playwright/test');
+
+test.only('Browser context test',async ({browser})=>
+{
+    const context = await browser.newContext();
+    const page = await context.newPage();
+
+    const userName = page.locator('#username');
+    const signInBtn = page.locator(".btn-md");
+    const cardTitles = page.locator(".card-title a");
+
+    await page.goto('https://rahulshettyacademy.com/loginpagePractise/');
+    console.log(await page.title());
+    await userName.fill('rahulshetty'); // Id -> tagname#Id or #Id 
+    await page.locator("[name='password']").fill('Learning@830$3mK2'); // Css based on attribute -> [attribute='value'] 
+    await signInBtn.click(); // Class -> tagname.class or .class
+    console.log(await page.locator("[style*='block']").textContent());
+    await expect(page.locator("[style*='block']")).toContainText("Incorrect username/password.");
+    await userName.fill('rahulshettyacademy'); // Id -> tagname#Id or #Id 
+    await signInBtn.click(); // Class -> tagname.class or .class
+    console.log(await cardTitles.nth(1).textContent());
+    console.log(await cardTitles.first().textContent());
+    console.log(await cardTitles.allTextContents());
+});
+
+test('Page fixture test',async ({browser, page})=>
+{
+    //when you just need default context and page, send page as fixture in function
+    //const context = await browser.newContext();
+    //const page = await context.newPage();
+    await page.goto('https://google.com');
+    console.log(await page.title());
+    await expect(page).toHaveTitle('Google');
+});
