617b0a8abe5a71e252455cc7a01f9c5795dc80d5
Viswa <bvsukanth@gmail.com>
2026-02-19 20:13:59 -0600

    2/19/26 - 1st checkin - API + WEB combined test cases

--- tests/WebAPI1.spec.js
+++ tests/WebAPI1.spec.js
@@ -1,21 +1,21 @@
 const {test, expect, request} = require('@playwright/test');
 const loginPayload = {userEmail:"Tester@2223.com",userPassword:"Test@123"};
+const orderpayLoad = {orders:[{country:"Cuba",productOrderedId:"6960eac0c941646b7a8b3e68"}]};
 let token;
+let orderId;
+const {APIUtils} = require('./utils/APIUtils');
+let response;
 
 test.beforeAll(async ()=>{
     
+   
+    //Login API
     const apiContext = await request.newContext();
-   const logingResponse = await apiContext.post("https://rahulshettyacademy.com/api/ecom/auth/login",
-        {
-        data:loginPayload
-})
+    const apiUtils = new APIUtils(apiContext, loginPayload);
+    response = await apiUtils.createOrder(orderpayLoad);
+    
 
-expect(logingResponse.ok()).toBeTruthy();
-const loginResponseJson = await logingResponse.json();
-token = loginResponseJson.token;
-console.log(token);
-
-
+   
 });
 
 test.beforeEach(()=>{
@@ -24,99 +24,38 @@ test.beforeEach(()=>{
 
 });
 
-test('Browser context',async ({browser, page})=>
+test('Place order',async ({browser, page})=>
 {
+    
+    
 
     page.addInitScript(value => {
 
         window.localStorage.setItem('token', value);
-    }, token)
+    }, response.token)
 
     //const context = await browser.newContext();
     //const page = await context.newPage();
 
 
     await page.goto("https://rahulshettyacademy.com/client");
-    // await page.locator("#userEmail").fill("Tester@2223.com");
-    // await page.locator("[formcontrolname='userPassword']").fill("Test@123");
-    // await page.locator(".login-btn").click();
-
-    const products = page.locator(".card-body b");
-    const productName = "ZARA COAT 3";
-
-
-    //1st wait mechanism in playwright to print allTextContents() directly instead of giving nth or first
-    await page.waitForLoadState('networkidle');
-
-    //2nd wait mechanism in playwright
-    await page.locator(".card-body b").first().waitFor();
-
-    console.log(await page.locator(".card-body b").allTextContents());
-
-    const count = await products.count();
-
-    for(let i=0; i<count;i++)
-    {
-        if(await page.locator(".card-body b").nth(i).textContent() == productName)
-        {
-            //just text finding
-            await page.locator(".card-body").nth(i).locator("text= Add To Cart").click();
-            break;
-        }
-    }
-
-    await page.locator("[routerlink='/dashboard/cart']").click();
-    //expect(await page.locator(".cartSection h3")).toContainText(productName);
-
-    await page.locator("div li").first().waitFor();
-
-    //text finding based on tag
-    const bool = await page.locator("h3:has-text('ZARA COAT 3')").isVisible();
-    expect(bool).toBeTruthy();
-
-    
-    
-    await page.locator("button:has-text('Checkout')").click();
-
-    //pressSequentially can be done with delay also - await page.locator("[placeholder*='Country']").pressSequentially("ind", { delay: 150 });
-    await page.locator("[placeholder='Select Country']").pressSequentially("Ind");
-    const results = page.locator(".ta-results");
-    await results.waitFor();
-    const optionsCount = await results.locator('button').count();
-
-    for(let i=0; i< optionsCount ;i++)
-    {
-        const text = await results.locator("button").nth(i).textContent();
-        if(text.trim() === "India")
-        {
-            await results.locator("button").nth(i).click();
-            break;
-        }
-    }
-
-    await expect(page.locator(".user__name label")).toHaveText("Tester@2223.com");
-
-    await page.locator(".action__submit").click();
-    await expect(page.locator(".hero-primary")).toHaveText(" Thankyou for the order. ");
-
-    const orderID = await page.locator(".em-spacer-1 .ng-star-inserted").textContent();
-    console.log("Order ID : " + orderID);
+   
 
     await page.locator("button[routerlink*='myorders']").click();
 
     await page.locator("tbody").waitFor();
     const rows = await page.locator("tbody tr");
- 
- 
+
+    console.log(response);
     for (let i = 0; i < await rows.count(); ++i) {
         const rowOrderId = await rows.nth(i).locator("th").textContent();
-        if (orderID.includes(rowOrderId)) {
+        if (response.orderId.includes(rowOrderId)) {
             await rows.nth(i).locator("button").first().click();
             break;
         }
     }
     const orderIdDetails = await page.locator(".col-text").textContent();
-    expect(orderID.includes(orderIdDetails)).toBeTruthy();
+    expect(response.orderId.includes(orderIdDetails)).toBeTruthy();
         
 
     //await page.pause();
